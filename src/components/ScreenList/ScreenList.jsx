c84cf86e12ec1e219f6a35e923a0a1da213e9ce5
Jabin King <jabin@mister-king.co.uk>
2020-10-17 02:45:44 +0100

    Search filtering

--- src/components/ScreenList/ScreenList.jsx
+++ src/components/ScreenList/ScreenList.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FilterResults from 'react-filter-search';
 import Headline from '../Headline/Headline';
 import Record from '../Record/Record';
 
 import styles from './ScreenList.module.scss';
+import InputContainer from '../InputContainer/InputContainer';
 
 const ScreenList = ({ records, setRecords }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   const handleUpdateRecord = (recordIndex, editedRecord) => {
     const updatedRecords = records;
     updatedRecords[recordIndex] = editedRecord;
@@ -23,24 +27,51 @@ const ScreenList = ({ records, setRecords }) => {
     setRecords([...updatedRecords]);
   };
 
+  const handleInputChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <>
       <Headline>Records List</Headline>
       <Headline type="tertiary">Page 1</Headline>
 
-      <div className={styles.recordlist}>
-        { records.map((record, i) => (
-          <Record
-            key={`record-${i}`}
-            artist={record.artist}
-            album={record.album_title}
-            year={record.year}
-            condition={record.condition}
-            recordIndex={i}
-            updateRecord={handleUpdateRecord}
-          />
-        ))}
-      </div>
+      <form onSubmit={() => null}>
+        <InputContainer
+          containerClassName={styles.search}
+          label="Search"
+          name="search"
+          type="text"
+          value={searchTerm}
+          handleInputChange={handleInputChange}
+        />
+
+        <FilterResults
+          value={searchTerm}
+          data={records}
+          renderResults={results => (
+            <div className={styles.recordlist}>
+              { !results.length
+                ? (
+                  <Headline className={styles.recordlist__empty} type="secondary">No results</Headline>
+                )
+                : (
+                  results.map((record, i) => (
+                    <Record
+                      key={`record-${i}`}
+                      artist={record.artist}
+                      album={record.album_title}
+                      year={record.year}
+                      condition={record.condition}
+                      recordIndex={i}
+                      updateRecord={handleUpdateRecord}
+                    />
+                  ))
+                )}
+            </div>
+          )}
+        />
+      </form>
     </>
   );
 };
